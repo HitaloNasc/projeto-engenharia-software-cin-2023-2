e3e6c71165ca62c304db92ef475c5d4ec36d147d
Ingrid Freire <100882928+ingridfsl@users.noreply.github.com>
2023-12-11 15:54:13 -0300

    add options button

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { CardContainer, StyledImage, StyledPrice, StyledName, StyledContent } from "./styles";
 import AddToCartButton from "../AddToCartButton";
-
+import OptionsButton from "../AddOptionsButton"; 
 const Card = (props) => {
   const formattedPrice = Number(props.price).toFixed(2);
 
   const handleAddToCart = () => {
     console.log("Adicionado ao carrinho!");
+  };
+  const handleEdit = () => {
+    console.log("Editar item:", props.name);
+  };
+
+  const handleDelete = () => {
+    console.log("Excluir item:", props.name);
   };
 
   return (
@@ -14,11 +21,18 @@ const Card = (props) => {
       <style>
         @import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,700&display=swap');
       </style>
-      <StyledImage src={props.image} />
-      <StyledContent>
+      {/* <StyledImage src={props.image} /> */}
+      <div style={{ display: 'flex' }}>
+        <StyledImage src={props.image} />
+        <div style={{ marginLeft: 0, padding: 0 }}> {/* Coloca o botão no lado direito */}
+          <OptionsButton onEdit={handleEdit} onDelete={handleDelete} />
+        </div>
+      </div>
+      <StyledContent >
         <StyledName>{props.name}</StyledName>
         <StyledPrice>R$ {formattedPrice}</StyledPrice>
         <AddToCartButton onClick={handleAddToCart} />
+        {/* <OptionsButton onEdit={handleEdit} onDelete={handleDelete} /> */}
       </StyledContent>
     </CardContainer>
   );
