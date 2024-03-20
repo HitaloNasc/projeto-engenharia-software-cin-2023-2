7edc4410e683762c857932a818d791fab9eb0fca
Douglas Silva <thedouglasilva@gmail.com>
2024-03-19 22:54:02 -0300

    feat: add form validation

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, IconButton, Hidden } from '@mui/material';
 import ArticleIcon from '@mui/icons-material/Article';
+import AddIcon from '@mui/icons-material/Add';
 import { Link } from 'react-router-dom';
 import Logo from './logo.png';
 
@@ -12,6 +13,14 @@ function Navbar() {
           <Typography variant="h8" component={Link} to="/dashboard" sx={{ flexGrow: 1, textDecoration: 'none', color: 'inherit' }}>
             <img src={Logo} alt="Logo" style={{ height: '50px', marginRight: '10px' }} />
           </Typography>
+          <IconButton color="inherit" aria-label="history" component={Link} to="/dashboard" style={{ marginRight: '5px' }}>
+            <AddIcon />
+          </IconButton>
+          <Hidden smDown>
+            <Typography variant="h6" component={Link} to="/dashboard" style={{ textDecoration: 'none', color: 'inherit', marginRight: '8px' }}>
+              Nova Análise
+            </Typography>
+          </Hidden>
           <IconButton color="inherit" aria-label="history" component={Link} to="/results" style={{ marginRight: '5px' }}>
             <ArticleIcon />
           </IconButton>
